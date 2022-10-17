515a879ae0af2eddcafe3a90c4266db4fea15e3a
Rafael <rafaelwvs16@icloud.com>
2022-10-17 12:47:08 -0300

    testes em C

---
+++ arquivo/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int entrada;
+    printf("digite um numero: \n");
+    scanf("%d", &entrada);
+    entrada++;
+    printf("voce tem: %d ", entrada, "reais %d");
+
+    return 0;
+}
---
+++ teste/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+ int main ()
+ {
+    int primeiraVariavel = 10;
+    int segundaVariavel = 23;
+    int resultado;
+-
+    resultado = primeiraVariavel + segundaVariavel+ 1 ;
+
+    printf("primeira variavel: %d\n", primeiraVariavel);
+    printf("segunda variavel: %d\n", segundaVariavel);
+    printf("% resultdo %d\n", resultado);
+    printf("primeira  %d, segunda %d , resultado %d", primeiraVariavel, segundaVariavel, resultado);
+
+    return 0;
+}
