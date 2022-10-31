c03b7a493f8f37da7b1193a66bb944a1b8cf0aa2
Rafael <Rafaelwvs16@gmail.com>
2022-10-31 15:14:51 -0300

    nao foi possivel debugar no mac

---
+++ teste.c
@@ -0,0 +1,23 @@
+int main()
+(
+    int valor;
+
+    printf("entre com um valor: ");
+    scanf("%d\n", &valor);
+    printf("voce entrou com o valor: %d\n", valor);
+
+    if( valor == 220)
+        printf( "o valor e", valor \n);
+
+    if(valor >10)(
+        printf("o valor e maior que 10\n");
+    else
+        printf("o valor e menor ou igual que 10\n");
+
+
+   )
+
+    return 0;
+
+)
+
