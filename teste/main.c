ee1fb5823773b0b1e7692e3cb8bb38c21a2a4883
Rafael <rafaelwvs16@icloud.com>
2022-11-18 22:21:02 -0300

    entradas

--- teste/main.c
+++ teste/main.c
@@ -1,19 +1,59 @@
-#include <stdio.h>
-#include <stdlib.h>
+sg2#include
+
+#include
+
+int main()
+{
+    float v1;
+    float v2;
+    char op;
+    float resultado;
+
+    //  inserindo dados//
+    printf("Entre com o primeiro valor: ");
+    scanf("%f", &v1);
+
+    printf("Entre com o operador desejado");
+    scanf(" %c", &op);
+
+    printf("Entre com o segundo valor: ");
+    scanf("%f", &v2);
+
+    //printf("%f %c %f", v1, op, v2);//
+
+    // imprimindo resultados//
+
+    switch(op){
+
+        case '+':
+            resultado = v1 + v2;
+
+            break;
+        case '-':
+            resultado = v1 - v2;
+
+            break;
+        case '*':
+            resultado = v1 * v2;
+
+            break;
+        case '/':
+            resultado = v1 / v2;
+            break;
+        default:
+            resultado = ("operador inválido")
+        return 1;
 
 
- int main ()
- {
-    int primeiraVariavel = 10;
-    int segundaVariavel = 23;
-    int resultado;
--
-    resultado = primeiraVariavel + segundaVariavel+ 1 ;
+    }
 
-    printf("primeira variavel: %d\n", primeiraVariavel);
-    printf("segunda variavel: %d\n", segundaVariavel);
-    printf("% resultdo %d\n", resultado);
-    printf("primeira  %d, segunda %d , resultado %d", primeiraVariavel, segundaVariavel, resultado);
-
+    int main()
+{
+    int contador = 0;
+    while (contador <10) {
+        printf("valor do contador; %d\n"contador);
+        contador++;
+    }
     return 0;
+
 }
